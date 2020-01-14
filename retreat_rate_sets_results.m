7b649700722346e6fc0672e3fe380d4b3e3bbe9f
ddileonardo <39922065+ddileonardo@users.noreply.github.com>
2020-01-14 16:09:48 -0600

    Update retreat_rate_sets_results.m
    add shoreface slope calc

--- retreat_rate_sets_results.m
+++ retreat_rate_sets_results.m
@@ -3,12 +3,23 @@ clc
 for ii = 1:length(output)
     
     ii
-    rate = output{ii,1}.dx_tdt(output{ii,1}.dx_tdt>0);
-    fprintf(['shoreface toe rate is ' num2str(median(rate)) '\n'])
+    %     rate = output{ii,1}.dx_tdt(output{ii,1}.dx_tdt>0);
+    %     fprintf(['shoreface toe rate is ' num2str(median(rate)) '\n'])
+    %
+    %     rate = output{ii,1}.dx_sdt(output{ii,1}.dx_sdt>0);
+    %     fprintf(['shoreline rate is ' num2str(median(rate)) '\n'])
+    %
+    for jj = 1:length(output{ii,1}.dx_sdt(1,:))
+    s_sf(jj) = nanmean(output{ii,1}.d_sf./(double(output{ii,1}.x_s_save(output{ii,1}.dx_sdt(:,jj)>0,jj))- double(output{ii,1}.x_t_save(output{ii,1}.dx_sdt(:,jj)>0,jj))));
     
-    rate = output{ii,1}.dx_sdt(output{ii,1}.dx_sdt>0);
-    fprintf(['shoreline rate is ' num2str(median(rate)) '\n'])
+    end
+    %fprintf(['shoreface slope is ' num2str(nanmedian(s_sf(:))) '\n'])
+    plot(s_sf)
     
+    
+    %     width = double(output{ii,1}.x_b_save(output{ii,1}.dx_sdt>0)) - double(output{ii,1}.x_s_save(output{ii,1}.dx_sdt>0));
+    %     plot(width)
+    %
     pause
     
 end
